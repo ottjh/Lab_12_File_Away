ccc02c59065ef168e3ba77f99d853cf1f8834dd8
jhott <ottjh@uc.mail.edu>
2024-11-28 10:48:06 -0500

    Seemingly working DataSaver

--- src/DataSaver.java
+++ src/DataSaver.java
@@ -14,15 +14,20 @@ public class DataSaver
         ArrayList<String> list = new ArrayList<String>();
         Scanner sc = new Scanner(System.in);
         boolean done = false;
+        String filename;
+        String filename2;
 
-        while
+        do
         {
-
+            addToList(sc, list);
             done = SafeInput.getYNConfirm(sc, "Do you want to quit inputting records? (Y/N)");
-        } (!done)
+        }  while (!done);
 
+        filename = SafeInput.getNonZeroLenString(sc, "Enter the desired filename");
+        filename2 = filename + ".csv";
+        System.out.println(filename2);
         File workingDirectory = new File(System.getProperty("user.dir"));
-        Path file = Paths.get(workingDirectory.getAbsolutePath() + "\\src\\main\\java\\DataSaver");
+        Path file = Paths.get(workingDirectory.getPath() + "\\src", filename2);
 
         try
         {
@@ -31,7 +36,8 @@ public class DataSaver
             BufferedWriter writer =
                     new BufferedWriter(new OutputStreamWriter(out));
 
-            for (String rec : recs) {
+            for (String rec : list)
+            {
                 writer.write(rec, 0, rec.length());
 
                 writer.newLine();
@@ -45,9 +51,18 @@ public class DataSaver
             e.printStackTrace();
         }
     }
+
+    //Here is a method to add everything
     public static void addToList (Scanner sc, ArrayList<String> list)
     {
-        String firstName= SafeInput.getNonZeroLenString(sc, "Please enter your first name: ");
+        String firstName= SafeInput.getNonZeroLenString(sc, "Please enter your first name");
+        String lastName= SafeInput.getNonZeroLenString(sc, "Please enter your last name");
+        String idNumber= SafeInput.getNonZeroLenString(sc, "Please enter your 6 digit id number (000001-999999)"); //I could probably put in a regex function here, but I have like 40 hours of work to do this week.
+        String email = SafeInput.getNonZeroLenString(sc, "Please enter your email"); //Dont have time to regex this.
+        int birthYear = SafeInput.getRangedInt(sc, "Please enter the year of your birth", 1900,2024);
 
+        String listRecord = firstName + ", " + lastName + ", " + idNumber + ", " + email + ", " + birthYear;
+
+        list.add(listRecord);
     }
 }
